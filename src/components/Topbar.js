73183caa3ed0ec3d45a9333e512107fbb62ffab9
Gustave Dassonville <gustave@dassonville.io>
2021-04-27 21:08:03 +0200

    some basic changes
    _ init of react
    _ deletion of unused files
    _ fonts
    _ few basic components

---
+++ src/components/Topbar.js
@@ -0,0 +1,7 @@
+import "./Topbar.css";
+
+function Topbar() {
+  return <div className="topbar">Topbar</div>;
+}
+
+export default Topbar;
---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Le beau titre h1</h1>
+      <h2>Le beau titre h2</h2>
+      <h3>Le beau titre h3</h3>
+    </div>
+  );
+}
+
+export default Home;
